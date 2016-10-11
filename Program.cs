1d985bb4a81f5978f2ab08e7ae44c756f2c7b997
Cindy Ngien <cindyngien@gmail.com>
2016-10-11 14:06:35 -0700

    Great Number Game using nancy_fx. C#

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System.IO;
+using Microsoft.AspNetCore.Hosting;
+
+namespace GNG
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            IWebHost host = new WebHostBuilder()
+                .UseIISIntegration()
+                .UseContentRoot(Directory.GetCurrentDirectory())
+                .UseKestrel()
+                .UseStartup<Startup>()
+                .Build();
+            host.Run();
+                
+        }
+    }
+}
